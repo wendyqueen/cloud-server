e1b122f40af832f40091c56585a748f16c6de7eb
wendyqueen <wanqiuzhengcs@gmail.com>
2025-07-12 20:02:10 +0800

    fix CI

--- src/validators.js
+++ src/validators.js
@@ -12,8 +12,6 @@ const VALUE_MAX_LENGTH = 100000;
 const USERNAME_MAX_LENGTH = 20;
 /** Minimum length of usernames, inclusive. */
 const USERNAME_MIN_LENGTH = 1;
-/** Regex for usernames to match. Letters, numbers, -, and _ */
-const USERNAME_REGEX = /^[a-z0-9_-]+$/i;
 
 /**
  * @param {unknown} username
